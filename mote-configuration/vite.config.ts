55c653100fb34d2f0b7c6a41e9bcd6c0248b1931
spike-h <spikerman101@gmail.com>
2026-05-20 14:41:16 -0400

    chaning mote-core to mote

--- mote-configuration/vite.config.ts
+++ mote-configuration/vite.config.ts
@@ -4,7 +4,7 @@ import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/mote-core/configuration/",
+    base: "/mote/configuration/",
     plugins: [svelte(), wasm()],
     server: {
         fs: {
